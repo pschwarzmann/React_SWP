4705a8d7488d00f2d7ee40499df4ab0a345518a0
pschwarzmann <paul.schwarzmann@student.htldornbirn.at>
2025-01-10 09:09:56 +0100

    neuer push (nichts funktioniert)

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Button.module.css'
+
+export default function Button(props) {
+    return(
+        <div className={styles.main}> {props.name} is {props.age} years old</div>
+    )
+}

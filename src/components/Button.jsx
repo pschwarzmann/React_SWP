2f72a9356d55407195cb0335d349b34fef431ba5
pschwarzmann <paul.schwarzmann@student.htldornbirn.at>
2025-01-17 08:28:42 +0100

    funktioniert (die button aufgabe)
    was geht

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Button.module.css'
 
 export default function Button(props) {
-    return(
-        <div className={styles.main}> {props.name} is {props.age} years old</div>
-    )
+        const [isOn, setIsOn] = useState();
+    return (
+        <div className={isOn?styles.isOn:styles.isOff}
+        onClick={() => {
+            alert("clicked");
+            setIsOn(!isOn);
+        }}> 
+        
+        {props.name} is {props.age} years old and is {isOn?"On":"Off"}
+        
+        </div>
+    );
 }
